896d97060ad5ef7fcf06ee86542cff5816b55b9d
DannyHack <65183782+CarlosDelRosario7@users.noreply.github.com>
2023-09-16 08:36:04 -0400

    incluir instalación de docker-compose

--- install.sh
+++ install.sh
@@ -16,11 +16,17 @@ sudo apt install apt-transport-https ca-certificates curl gnupg lsb-release soft
 echo -ne "[*] Instalado docker..."
 sudo dpkg -i ./containerd.io_1.6.9-1_amd64.deb ./docker-ce-cli_24.0.6-1\~debian.11\~bullseye_amd64.deb ./docker-ce_24.0.6-1\~debian.11\~bullseye_amd64.deb ./docker-buildx-plugin_0.11.2-1\~debian.11\~bullseye_amd64.deb ./docker-compose-plugin_2.6.0\~debian-bullseye_amd64.deb
 
+echo -ne "[*] Instalando docker-compose..."
+wget https://github.com/docker/compose/releases/download/v2.6.0/docker-compose-linux-x86_64
+chmod +x docker-compose-linux-x86_64
+sudo mv docker-compose-linux-x86_64 /usr/local/bin/docker-compose
+
 echo -ne "[*] Configurando docker..."
 sudo service docker start # o sudo systemctl enable docker && sudo systemctl start docker
 sudo usermod -aG docker $USER # añadir usuario al grupo docker
 
 echo -n "[*] Comprobando instalación..."
 sudo docker run hello-world
+sudo docker-compose --version
 
 echo -ne "[*] Instalación finalizada"
